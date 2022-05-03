fb18c2ef89079ab476a69e9ef519d964f9abee6b
thedevak <akthedev@outlook.com>
2022-05-03 12:02:05 +0530

    Title Change

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,14 @@ export default function Home({posts, events}) {
   return (
     <>
       <Head>
-        <title>Tech Blog</title>
+        <title>Hello Blog</title>
         <meta name="description" content="Keep up to date with the latest trends in tech" />
         <link rel="icon" href="/favicon.ico" />
         {/* You can add more metadata here, like open graph tags for social media, etc */}
       </Head>
 
       <div className="container pt-5">
-        <h1 className="text-center pb-5">Tech Blog</h1>
+        <h1 className="text-center pb-5">Hello Blog</h1>
         <div className="row">
           <div className="col-lg-8">
             <h2 className="pb-3">Our blog posts</h2>
